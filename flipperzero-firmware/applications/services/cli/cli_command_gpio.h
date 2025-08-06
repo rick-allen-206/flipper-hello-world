5daf1bb3d24e43c06aee2acf70f225502b1abe62
lasky <rickallen206@gmail.com>
2025-08-06 00:53:00 -0700

    init commit

--- flipperzero-firmware/applications/services/cli/cli_command_gpio.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <toolbox/pipe.h>
-
-void cli_command_gpio(PipeSide* pipe, FuriString* args, void* context);
--- flipperzero-firmware/build/f7-firmware-D/toolbox/version.inc.h
+++
@@ -1,8 +0,0 @@
-#define GIT_COMMIT "d1c3b0bc"
-#define GIT_BRANCH "dev"
-#define VERSION "unknown"
-#define BUILD_DIRTY 0
-#define GIT_ORIGIN "https://github.com/flipperdevices/flipperzero-firmware.git"
-#define BUILD_DATE "21-05-2025"
-#define TARGET 7
-#define FIRMWARE_ORIGIN "Official"
